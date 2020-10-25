961a92aaf8265ca56dacf1e202a01789a6cacb33
VoyagerWSH <VoyagerWSH@icloud.com>
2020-10-25 15:12:27 -0400

    random

--- model_app/frontend/src/containers/Home.js
+++ model_app/frontend/src/containers/Home.js
@@ -1,74 +1,24 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-import { AboutImg, DevelopmentBlogImg, SimulatorImg, TeamImg, MapImg} from '../images'
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './Home.css';
 
-class Home extends Component{
-
-    constructor(props) {
-        super(props);
-
-        this.links = [
-            {
-                id: 0,
-                name:'About',
-                image: AboutImg,
-                description:'Overview of our Covid-19 Modeling project'
-            },
-            {
-                id: 1,
-                name:'Team',
-                image: TeamImg,
-                description:'Our team composition and leadership'
-            },
-	    {
-                id: 2,
-                name:'Map Visualization',
-                image: MapImg,
-                description:'U.S. Map Visualization of COVID-19'
-            },
-            {
-                id: 3,
-                name:'Simulator',
-                image: SimulatorImg,
-                description:'Use this page to run the simulator we create for Covid-19 Modeling'
-            },
-            {
-                id: 4,
-                name:'Development Blog',
-                image: DevelopmentBlogImg,
-                description:'The page displaying our process of simulator development and expectation on future update'
-            },
-        ];
-    }
+class Home extends Component {
 
     render() {
 
-        const menu = this.links.map((link) => {
-            let imagStyle = {maxHeight: 128,
-                maxWidth: 128};
-            return (
-                <div key={link.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={link.image} style={imagStyle} alt={link.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading>{link.name}</Media>
-                            <p>{link.description}</p>
-                        </Media>
-                    </Media>
-                </div>
-            );
-        });
-
         return (
-            <div className="container">
-                <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+            <React.Fragment>
+                <CssBaseline />
+                <div className="image-box">
+                    <div className="header-text">
+                        <h1>Interactive Disease Simulation</h1>
+                        <Button variant="contained" color="primary" href="/simulator">
+                            Explore Simulation
+                            </Button>
+                    </div>
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
