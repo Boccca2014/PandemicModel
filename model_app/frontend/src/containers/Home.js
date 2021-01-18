35b1c251fad15019cbc6f89110ba61472e3bdac6
Simone Bliss <sea.monester99@gmail.com>
2021-01-17 22:06:15 -0800

    css converted to jsx/materialUI

--- model_app/frontend/src/containers/Home.js
+++ model_app/frontend/src/containers/Home.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import './Home.css';
+import {withStyles} from '@material-ui/core';
+
+//import './Home.css';
+
+const styles = {
+    imageBox: {
+        ///* Here's the trick */
+        background: "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))", 
+        backgroundImage: "url('https://9to5mac.com/wp-content/uploads/sites/6/2020/01/Coronavirus.jpg?quality=82&strip=all')",
+        backgroundSize: "cover",
+        //backgroundImage: "url('https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg')",
+    
+        ///* Here's the same styles we applied to our content-div earlier */
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+
+        "&:hover": {
+            boxShadow: "inset 0 0 0 100vw rgba(0,0,0,0.5)",
+        },
+    },
+    
+    Button: {
+        "&:hover": {
+            color: "black",
+            backgroundColor: "#66FCFF",
+        },
+    },   
+};
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
-
+        const {classes} = this.props;
         return (
             <React.Fragment>
                 <CssBaseline />
-                <div className="image-box">
-                    <div className="header-text">
+                <div className={classes.imageBox}>
+                    <div className={classes.headerText}>
                         <h1>Interactive Disease Simulation</h1>
-                        <Button className="Button" variant="contained" color="primary" href="/simulator">
+                        <Button className={classes.Button} variant="contained" color="primary" href="/simulator">
                             Explore Simulation
                         </Button>
                     </div>
@@ -22,4 +55,4 @@ class Home extends Component {
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
